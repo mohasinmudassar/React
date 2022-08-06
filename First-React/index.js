57a030abd209cf3fc0e5baebccb42dd2c0d7563e
mohasinmudassar <mohasinmudassar16@gmail.com>
2022-08-07 00:32:11 +0500

    commiting index.js file

--- First-React/index.js
+++ First-React/index.js
@@ -1,1 +1,24 @@
-ReactDOM.render(<h1>Hello New Guy...</h1>, document.getElementById("root"))
+//ReactDOM.render(<ul><li>Coffee</li><li>Milk</li><li>Tea</li></ul>, document.getElementById("root"));
+/*const h1 = document.createElement("h1");
+h1.textContent = "Hello World";
+h1.className ="header";
+document.get    ElementById("root").append(h1);*/
+
+const page = (
+    <nav>
+        <h1>My Page</h1>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+    </nav>
+
+    )
+  
+ReactDOM.render(
+    page,
+    document.getElementById("root")
+    )
+
+
