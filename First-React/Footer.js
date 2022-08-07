299bceb48b5e6b7c649b6f4c922ce2af9cb415e6
mohasinmudassar <mohasinmudassar16@gmail.com>
2022-08-07 23:36:11 +0500

    all

---
+++ First-React/Footer.js
@@ -0,0 +1,8 @@
+
+export default function Footer(){
+    return(
+        <footer className="footer">
+                <small>"© 2022 Mohasin Mudassar development. All rights reserved."</small>
+        </footer>
+    )
+}
