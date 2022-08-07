c03832c40aa595549af185a447aaf1b903186ebb
mohasinmudassar <mohasinmudassar16@gmail.com>
2022-08-08 02:40:29 +0500

    basic project

---
+++ second-react/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Mohasin is here!!!</h1>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <h1>Mohasin </h1>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
