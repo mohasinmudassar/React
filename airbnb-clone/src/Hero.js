416668c7ba87917c0b3a7ff14c9aea93ef743173
mohasinmudassar <mohasinmudassar16@gmail.com>
2022-08-10 00:11:50 +0500

    complete styling of here section

--- airbnb-clone/src/Hero.js
+++ airbnb-clone/src/Hero.js
@@ -3,11 +3,11 @@ import "./style.css";
 
 export default function Hero() {
     return(
-        <hero >
+        <section className = "hero-all">
             <img className= "hero-img" src = '../main.png' alt = 'main' />
             <h1 className="hero-h1">Online Experiences</h1>
             <p className="hero-p" >Join unique interactive activities led by 
             one-of-a-kind hosts—all without leaving home.</p>
-        </hero>
+        </section>
     )
 } 
