af66bd1a2a0e9d3bf4533c8a3fab79a9e72f3e60
mohasinmudassar <mohasinmudassar16@gmail.com>
2022-08-10 00:52:34 +0500

    first card hardcode

--- airbnb-clone/src/Card.js
+++ airbnb-clone/src/Card.js
@@ -3,8 +3,16 @@ import "./style.css";
 
 export default function Card() {
     return(
-        <card>
-            <h1>Card Component</h1>
-        </card>
+        <div className="card">
+            <img src='../person-1.png' alt='person-1' className="card-image"/>
+            <div className="card-stats">
+                <img src='../Star 1.png' alt='star' className="card-star"/>
+                <span> 5.0 </span>
+                <span className="grey"> (6) • </span>
+                <span className="grey">USA</span>
+            </div>
+            <p>Life lessons with Katie Zaferes</p>
+            <p ><span className="bold">From 136$ </span>/ person</p>
+        </div>
     )
 }
