0c3a5c20d362456ea765ca8c5a5a6e9c180ebe10
mohasinmudassar <mohasinmudassar16@gmail.com>
2022-08-10 00:23:40 +0500

    adding images and card component

--- airbnb-clone/src/App.js
+++ airbnb-clone/src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import Navbar from './Navbar';
 import Hero from './Hero';
+import Card from './Card';
 
 
 export default function App() {
   return (
     <div>
       <Navbar/>
-      <Hero/>
+      <Card/>
     </div>
   );
 }
---
+++ airbnb-clone/src/Card.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./style.css";
+
+export default function Card() {
+    return(
+        <card>
+            <h1>Card Component</h1>
+        </card>
+    )
+}
