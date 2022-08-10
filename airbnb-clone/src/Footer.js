674df59537e2c64637435a361784d41104eb1ffb
mohasinmudassar <mohasinmudassar16@gmail.com>
2022-08-10 21:45:28 +0500

    Footer

---
+++ airbnb-clone/src/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.css';
+
+
+export default function Footer(){
+    return(
+            <footer className="footer">
+                    <small>"© 2022 Mohasin Mudassar development. All rights reserved."</small>
+            </footer>
+        )
+    
+}
