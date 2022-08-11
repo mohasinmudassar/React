dc8eaa9e966598891792a12e160e191b9c2db115
mohasinmudassar <mohasinmudassar16@gmail.com>
2022-08-12 01:31:51 +0500

    card props done of airbnb

--- airbnb-clone/src/App.js
+++ airbnb-clone/src/App.js
@@ -3,37 +3,30 @@ import Navbar from './Navbar';
 import Hero from './Hero';
 import Card from './Card';
 import Footer from './Footer';
-import Contact from './Contact';
+
 
 
 export default function App() {
   return (
     <div>
       <Navbar/>
-      <Contact
-        img = "../cat1.jpeg"
-        name="Mr. Whiskerson"
-        phone="(212) 555-1234"
-        email="cat1@gmail.com"
-      /> 
-      <Contact
-        img = "../cat-2.jpeg"
-        name="Mr. tpmmy"
-        phone="(312) 555-4321"
-        email="cat2@gmail.com"
+      <Hero/>
+      <Card
+      image="../person-1.png"
+      rating="5.0"
+      reviews=" 42 "
+      country="England"
+      title="Stay at home and do nothing"
+      price="10"
       />
-      <Contact
-        img = "../cat-3.jpg"
-        name="Cutie"
-        phone="(412) 555-1234"
-        email="cat3@gmail.com"
-      />
-      <Contact
-        img = "../cat-4.jpeg"
-        name="Lala Cat"
-        phone="(412) 555-1234"
-        email="cat4@gmail.com"
-      />
+      <Card
+      image="../person-2.jpeg"
+      rating="4.0"
+      reviews="22 "
+      country="USA"
+      title="Stay at home and Live Life"
+      price="20"
+      />    
       
     </div>
   );
--- airbnb-clone/src/Card.js
+++ airbnb-clone/src/Card.js
@@ -1,18 +1,18 @@
 import React from "react";
 import "./style.css";
 
-export default function Card() {
+export default function Card(props) {
     return(
-        <div className="card">
-            <img src='../person-1.png' alt='person-1' className="card-image"/>
-            <div className="card-stats">
-                <img src='../Star 1.png' alt='star' className="card-star"/>
-                <span> 5.0 </span>
-                <span className="grey"> (6) • </span>
-                <span className="grey">USA</span>
+            <div className="card ">
+                <img src= {props.image} className="card-image"/>
+                <div className="card-stats">
+                    <img src='../Star 1.png' alt='star' className="card-star"/>
+                    <span> {props.rating} </span>
+                    <span className="grey"> {props.reviews} • </span>
+                    <span className="grey">{props.country}</span>
+                </div>
+                <p>{props.title}</p>
+                <p ><span className="bold">From {props.price}$ </span>/ person</p>
             </div>
-            <p>Life lessons with Katie Zaferes</p>
-            <p ><span className="bold">From 136$ </span>/ person</p>
-        </div>
     ) 
 }
