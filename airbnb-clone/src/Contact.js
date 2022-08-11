48b4150ccb51986cbb9472b400eed1e198a73188
mohasinmudassar <mohasinmudassar16@gmail.com>
2022-08-11 23:56:10 +0500

    icons

--- airbnb-clone/src/Contact.js
+++ airbnb-clone/src/Contact.js
@@ -7,13 +7,14 @@ export default function Contact() {
             <img src="../cat1.jpeg"/>
             <h3>Mr. Whiskerson</h3>
             <div className="info-group">
-                <img src="./images/phone-icon.png" />
+                <img src="../phone.png" />
                 <p>(212) 555-1234</p>
             </div>
             <div className="info-group">
-                <img src="./images/mail-icon.png" />
+                <img src="../mail.png" />
                 <p>mr.whiskaz@catnap.meow</p>
             </div>
         </div>
+        
     );
 }
