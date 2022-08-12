37c1462be65b45ea38444119c572c1cb6a2b3198
mohasinmudassar <mohasinmudassar16@gmail.com>
2022-08-13 02:01:20 +0500

    adding data.js file

--- airbnb-clone/src/App.js
+++ airbnb-clone/src/App.js
@@ -3,14 +3,20 @@ import Navbar from './Navbar';
 import Hero from './Hero';
 import Card from './Card';
 import Footer from './Footer';
-
+import data from "./Data";
 
 
 export default function App() {
+/*
+    const carddata = Data.map(data => {
+        return <Card image ={img.coverImg}  rating = {rat.rating} reviews = {rev.status(reviewCount)} location = {local.location} title = {tit.title} price = {pr.price} /> })
+*/    
   return (
     <div>
       <Navbar/>
       <Hero/>
+      
+  
       <Card
       image="../images/person-1.png"
       rating="5.0"
@@ -26,7 +32,7 @@ export default function App() {
       location="USA"
       title="Stay at home and Live Life"
       price={20}
-      />    
+      />   
       
     </div>
   );
