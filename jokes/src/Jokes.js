e7d3f57f5b25da6c908993908171d52dfb3b79bf
mohasinmudassar <mohasinmudassar16@gmail.com>
2022-08-13 23:22:27 +0500

    jokes

--- jokes/src/Jokes.js
+++ jokes/src/Jokes.js
@@ -5,7 +5,32 @@ import './index.css';
 export default function Jokes() {
     return(
         <div>
-            <h1>Hello World</h1>
+            <joke 
+            Setup: I got my daughter a fridge for her birthday.
+            Punchline: I can't wait to see her face light up when she opens it.
+            />
+            
         </div>
     )
 }
+
+
+---
+
+Setup: How did the hacker escape the police?
+Punchline: He just ransomware!
+
+---
+
+Setup: Why don't pirates travel on mountain roads?
+Punchline: Scurvy.
+
+---
+
+Setup: Why do bees stay in the hive in the winter?
+Punchline: Swarm.
+
+---
+
+Setup: What's the best thing about Switzerland?
+Punchline: I don't know, but the flag is a big plus!
