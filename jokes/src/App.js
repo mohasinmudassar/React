b65f6a80ee6c5677ca5e6f762448431b8c1e1ff8
mohasinmudassar <mohasinmudassar16@gmail.com>
2022-08-13 23:26:14 +0500

    jokes added

--- jokes/src/App.js
+++ jokes/src/App.js
@@ -5,7 +5,22 @@ import Jokes from './Jokes';
 export default function App() {
   return (
     <div >
-      <Jokes />
+      <Joke
+        setup: "How did the hacker escape the police?"
+        punchline: "He just ransomware!"
+      />
+      <Joke
+        setup: "Why don't pirates travel on mountain roads?"
+        punchline: "Scurvy."
+      />
+      <Joke
+        setup: "Why do bees stay in the hive in the winter?"
+        punchline: "Swarm."
+      />
+      <Joke
+        setup: "What's the best thing about Switzerland?"
+        punchline: "I don't know, but the flag is a big plus!"
+      />
     </div>
   );
 }
--- jokes/src/Jokes.js
+++ jokes/src/Jokes.js
@@ -2,35 +2,8 @@ import React from 'react';
 import './index.css';
 
 
-export default function Jokes() {
+export default function Joke() {
     return(
-        <div>
-            <joke 
-            Setup: I got my daughter a fridge for her birthday.
-            Punchline: I can't wait to see her face light up when she opens it.
-            />
-            
-        </div>
+        <h1>Joke Here</h1>
     )
 }
-
-
----
-
-Setup: How did the hacker escape the police?
-Punchline: He just ransomware!
-
----
-
-Setup: Why don't pirates travel on mountain roads?
-Punchline: Scurvy.
-
----
-
-Setup: Why do bees stay in the hive in the winter?
-Punchline: Swarm.
-
----
-
-Setup: What's the best thing about Switzerland?
-Punchline: I don't know, but the flag is a big plus!
