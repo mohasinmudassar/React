85abcaa745a81130f80a0267a2c9420f8d51cdfd
mohasinmudassar <mohasinmudassar16@gmail.com>
2022-08-13 23:29:27 +0500

    jokes completed

--- jokes/src/App.js
+++ jokes/src/App.js
@@ -5,21 +5,21 @@ import Jokes from './Jokes';
 export default function App() {
   return (
     <div >
-      <Joke
-        setup: "How did the hacker escape the police?"
-        punchline: "He just ransomware!"
+      <Jokes
+        setup = "How did the hacker escape the police?"
+        punchline = "He just ransomware!"
       />
-      <Joke
-        setup: "Why don't pirates travel on mountain roads?"
-        punchline: "Scurvy."
+      <Jokes
+        setup = "Why don't pirates travel on mountain roads?"
+        punchline = "Scurvy."
       />
-      <Joke
-        setup: "Why do bees stay in the hive in the winter?"
-        punchline: "Swarm."
+      <Jokes
+        setup = "Why do bees stay in the hive in the winter?"
+        punchline = "Swarm."
       />
-      <Joke
-        setup: "What's the best thing about Switzerland?"
-        punchline: "I don't know, but the flag is a big plus!"
+      <Jokes
+        setup = "What's the best thing about Switzerland?"
+        punchline = "I don't know, but the flag is a big plus!"
       />
     </div>
   );
