ff73609c44af14a962cf744eca47e3dd046863f0
mohasinmudassar <mohasinmudassar16@gmail.com>
2022-08-14 23:55:40 +0500

    update 3.0

--- airbnb-clone/src/App.js
+++ airbnb-clone/src/App.js
@@ -5,25 +5,24 @@ import Card from './Card';
 import Footer from './Footer';
 import data from "./Data";
 
-console.log(data);
-/*
+
 export default function App() {
 
-    const carddata = Data.map(data => {
+    const cards = data.map(data => {
         return <Card image ={image.coverImg}  rating = {rating.rating} reviews = {reviews.reviewCount} location = {location.location} title = {title.title} price = {price.price} /> })
    
   return (
     <div>
       <Navbar/>
       <Hero/>
-      {carddata}
+      {cards}
   
         
       
     </div>
   );
 }
-*/
+
 /*
 <Card
       image="../images/person-1.png"
