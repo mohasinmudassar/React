3f1a41c417f821518b6623cc603fac1b74b6e069
mohasinmudassar <mohasinmudassar16@gmail.com>
2022-08-16 01:09:37 +0500

    nav bar created

--- travel-journal/src/App.js
+++ travel-journal/src/App.js
@@ -1,8 +1,10 @@
+import Navbar from "./Navbar";
+
 function App() {
   return (
     <div>
       <header>
-        
+        <Navbar/>
         <h1>Moahsin</h1>
         
       </header>
---
+++ travel-journal/src/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./index.css";
+
+export default function Navbar (){
+    return (
+        <nav className="nav">
+            <img className="nav-img" src = "../photos/globe.png" />
+            <h2 className="nav-h2">my travel journal.</h2>
+        </nav>
+    );
+}
