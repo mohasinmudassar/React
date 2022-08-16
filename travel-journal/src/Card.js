618a944a979e9cc732a276d5b91fcb39281c6007
mohasinmudassar <mohasinmudassar16@gmail.com>
2022-08-16 23:01:48 +0500

    passing props with data

--- travel-journal/src/Card.js
+++ travel-journal/src/Card.js
@@ -1,23 +1,21 @@
 import React from "react";
 import "./index.css";
 
-export default function Card(){
+export default function Card(props){
     return(
         <div>
         <div className="card">
 
-                <img className=" card-image" src = "../photos/japan.jpeg"/>
+                <img className=" card-image" src = {props.imageUrl}/>
             <div className="card-info">
                 <div className="location-stats">
                     <img src='../photos/location.png' alt='location' className="card-location"/>
-                    <span className="loc-dest">JAPAN</span>
-                    <span className="grey"><u>View on Google Maps</u></span>
+                    <span className="loc-dest">{props.location}</span>
+                    <a className="grey" href={props.googleMapsUrl} target="_blank"><u>View on Google Maps</u></a>
                 </div>
-                    <h1 className="des-h1">Mount Fuji</h1>
-                    <span className = "des-date">12 Jan-2021 - 24 Jan,2021</span>
-                    <p className="des-text">Mount Fuji is the tallest mountain in Japan, standing at 
-                        3,776 meters (12,380 feet). Mount Fuji is the single most 
-                        popular tourist site in Japan, for both Japanese and foreign tourists.</p>    
+                    <h1 className="des-h1">{props.title}</h1>
+                    <span className = "des-date">{props.date}</span>
+                    <p className="des-text">{props.description}</p>    
             </div>
             </div>
             <hr className="hr" />
