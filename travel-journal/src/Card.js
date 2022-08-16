d21286ceb3d3eb8e268b98e34486efae4347b95b
mohasinmudassar <mohasinmudassar16@gmail.com>
2022-08-16 15:13:17 +0500

    card update 2.0

--- travel-journal/src/Card.js
+++ travel-journal/src/Card.js
@@ -3,18 +3,22 @@ import "./index.css";
 
 export default function Card(){
     return(
-        <div className="card">
-            <img src = "../photos/japan.jpeg" className="card-image"/>
+        <div>
+            <div className="card">
+                <img src = "../photos/japan.jpeg" className="card-image"/>
 
-            <div className="card-stats">
-                <img src='../photos/location.png' alt='location' className="card-location"/>
-                <span>japan</span>
-                <span>view on google maps</span>
+                <div className="card-stats">
+                    <img src='../photos/location.png' alt='location' className="card-location"/>
+                    <span>japan</span>
+                    <span>view on google maps</span>
+                </div>
+                <h1>Mount Fuji</h1>
+                <date>12-02-2022</date>
+                <p>msdshadkhsfkjkjfkjdfjkbgkjb</p>
+                
+
             </div>
-            <h1>Mount Fuji</h1>
-            <date>12-02-2022</date>
-            <p>msdshadkhsfkjkjfkjdfjkbgkjb</p>
-
+            <hr/>
         </div>
     )
 }
