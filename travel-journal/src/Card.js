c8b6f06b739190b182169be4278a2af5f73a724a
mohasinmudassar <mohasinmudassar16@gmail.com>
2022-08-16 17:06:05 +0500

    card update 3.0

--- travel-journal/src/Card.js
+++ travel-journal/src/Card.js
@@ -3,21 +3,23 @@ import "./index.css";
 
 export default function Card(){
     return(
-        <div>
-            <div className="card">
-                <img src = "../photos/japan.jpeg" className="card-image"/>
+        <div className="card">
 
-                <div className="card-stats">
+                <img className=" card-image" src = "../photos/japan.jpeg"/>
+
+                <div className="location-stats">
                     <img src='../photos/location.png' alt='location' className="card-location"/>
-                    <span>japan</span>
-                    <span>view on google maps</span>
+                    <span>JAPAN</span>
+                    <span className="grey"><u>View on Google Maps</u></span>
                 </div>
-                <h1>Mount Fuji</h1>
-                <date>12-02-2022</date>
-                <p>msdshadkhsfkjkjfkjdfjkbgkjb</p>
+                    <h1 className="des-h1">Mount Fuji</h1>
+                    <span className = "des-date">12 Jan-2021 - 24 Jan,2021</span>
+                    <p className="des-text">Mount Fuji is the tallest mountain in Japan, standing at 
+                        3,776 meters (12,380 feet). Mount Fuji is the single most 
+                        popular tourist site in Japan, for both Japanese and foreign tourists.</p>
+
                 
 
-            </div>
             <hr/>
         </div>
     )
