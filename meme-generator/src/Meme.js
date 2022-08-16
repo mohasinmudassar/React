4ff45ebb1c79661e286ed6041e84a5975410478b
mohasinmudassar <mohasinmudassar16@gmail.com>
2022-08-17 00:15:32 +0500

    creating a meme component

---
+++ meme-generator/src/Meme.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.css';
+
+export default function Form() {
+    return(
+        <form className='form'>
+            <input type="text" placeholder="Enter a meme text here" />
+            <input type="text" placeholder="Enter a meme text here" />
+            <button type="button">Click Me!</button>
+
+        </form>
+    )
+
+}
