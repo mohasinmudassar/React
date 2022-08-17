62d36f6a156f0e465e26e6ca7dd3ee3384be6cb1
mohasinmudassar <mohasinmudassar16@gmail.com>
2022-08-18 00:35:25 +0500

    adding meme data file

--- meme-generator/src/Meme.js
+++ meme-generator/src/Meme.js
@@ -2,8 +2,12 @@ import React from 'react';
 import './style.css';
 
 export default function Form() {
+    function handleClick(){
+        <a href="https://mail.google.com/mail/u/0/#inbox" target="_blank"></a>
+    }
     return(
         <main>
+            
             <form className='form'>
                 <input 
                     type="text" 
@@ -15,10 +19,8 @@ export default function Form() {
                     placeholder="Bottom text" 
                     className='form-input'
                 />
-                <button className='form-button' type="button">Get a new Meme Image</button>
+                <button className='form-button' onCLick ={handleClick} type="button">Get a new Meme Image</button>
             </form>
-        </main>
-        
+        </main>   
     )
-
 }
