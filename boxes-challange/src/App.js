abaa8b2fb0d0d87c30d1377fed5b82af92920669
mohasinmudassar <mohasinmudassar16@gmail.com>
2022-09-04 14:17:01 +0500

    usind stryling in react

--- boxes-challange/src/App.js
+++ boxes-challange/src/App.js
@@ -1,9 +1,25 @@
 import React from "react"
 import boxes from "./boxes";
-function App() {
+import "./index.css";
+
+function App(props) {
+  const [squares, setSquares]=React.useState(boxes)
+
+
+
+  const styles = {
+    backgroundColor: props.darkMode? '#222222' : '#cccccc'
+  }
+  const squareElements = squares.map(square => (
+    <div style={styles} className="box" key={square.id}>
+
+    </div>
+  ))
+
+
   return (
     <div >
-      <h1>Boxes will go here</h1>
+      {squareElements}
 
       
     </div>
