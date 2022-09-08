cad1db0b58d244c8ca0286587706c470186f3a8c
mohasinmudassar <mohasinmudassar16@gmail.com>
2022-09-09 01:37:47 +0500

    edited 3.0 for box challange

--- boxes-challange/src/Box.js
+++ boxes-challange/src/Box.js
@@ -1,7 +1,6 @@
 import React from 'react'
 
 export default function Box(props) {
-    const [on, setOn] = React.useState(props.on)
 
     const styles = {
         backgroundColor: on ? "#222222" : "transparent"
