c8f12671c898c149617ecf68811243c63c19acc4
mohasinmudassar <mohasinmudassar16@gmail.com>
2022-09-09 02:50:25 +0500

    forms in react-js

---
+++ forms/src/App.js
@@ -0,0 +1,16 @@
+
+
+function App() {
+  return (
+    <div >
+      <header>
+        <p>
+          Code will come here
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
