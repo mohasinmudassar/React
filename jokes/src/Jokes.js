49f5e8009f59907d3737807799c11a81249733d3
mohasinmudassar <mohasinmudassar16@gmail.com>
2022-09-09 02:01:19 +0500

    start in jokes with conditional rendering

--- jokes/src/Jokes.js
+++ jokes/src/Jokes.js
@@ -3,10 +3,18 @@ import './index.css';
 
 
 export default function Joke(props) {
+    const [isShown, setIsShown] = React.useState(false)
+    
+    function toggleShown(){
+        setIsShown(prevShown => !prevShown)
+    }
+    
+
     return(
         <div>
             {props.setup && <h3>Setup: {props.setup}</h3>}
             <p>Punchline: {props.punchline}</p>
+            <button cnCLick={toggleShown}>Show Punchline</button>
             <hr />
         </div>
         
